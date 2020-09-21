e677cbeb7b2163abb6183c8bf12d3f564a373da5
jstevens <jessica.stevens131@gmail.com>
2020-09-21 10:52:41 -0500

    list view

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('dates/', views.DateList.as_view(), name='date_index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Event
+from django.views.generic import ListView, 
+from django.views.generic.edit import CreateView
+
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+class  DateList(ListView):
+    model = Event
+
+class EventCreate(CreateView):
+    model = Event
+    fields = ['event_name', 'date', 'time', 'event_details']
